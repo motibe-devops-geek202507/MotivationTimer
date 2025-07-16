64d7f15f2990aff068b3652f9b8ed3407b4a1286
Yuuri Inoue <yuuri041125@gmail.com>
2025-07-16 11:18:00 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Hello Bootstrap with Vite!</h1>
+      <button className="btn btn-success">Click Me</button>
+    </div>
+  );
+}
+
+
+export default App
