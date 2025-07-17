6e42ca283d62d28f71cfda5908341ea2fe87da86
Yuuri Inoue <yuuri041125@gmail.com>
2025-07-17 21:59:18 +0900

    loginページを軽く追加。

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div>
+      <form action="submit" className="container flex">
+        <input type="text" />
+        <input type="password" />
+        <button>ログイン</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login
