6dedca4078b9a9a6d771a5a18ab7b6f73a1f2f8a
Yuuri Inoue <yuuri041125@gmail.com>
2025-07-17 17:59:23 +0900

    Topページの基本リンクを作成

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
-    <div className="container mt-5">
-      <h1 className="text-primary">Home</h1>
+    <div className="container py-4 px-3 mx-auto">
+      <h1 className="text-primary mx-4">モチベーションタイマーアプリ</h1>
+
+      <button className="btn btn-primary" onClick={() => navigate('/Login')}>
+        ログイン・アカウント作成へ
+      </button>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
